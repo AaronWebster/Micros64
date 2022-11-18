0598c9c46e488c86203ac0c4e4911deb5081798a
Aaron Webster <webstera@google.com>
2022-11-18 11:23:34 -0800

    Use macro to generate functions.

--- src/Micros64.h
+++ src/Micros64.h
@@ -6,10 +6,21 @@
 #include <Arduino.h>
 #include <inttypes.h>
 
+#define MAKE_FUNCTION(name, now)                    \
+  uint64_t name {                                   \
+    static uint32_t low, high;                      \
+    uint32_t new_low = now;                         \
+    if (new_low < low) high++;                      \
+    low = new_low;                                  \
+    return static_cast<uint64_t>(high) << 32 | low; \
+  }
+
 // Returns an unsigned 64-bit monotonic microseconds counter.
-uint64_t Micros64();
+MAKE_FUNCTION(Micros64(), micros())
 
 // Returns an unsigned 64-bit monotonic milliseconds counter.
-uint64_t Millis64();
+MAKE_FUNCTION(Millis64(), millis())
+
+#undef MAKE_FUNCTION
 
 #endif  // MICROS64_H_
